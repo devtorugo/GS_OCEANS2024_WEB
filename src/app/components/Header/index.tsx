ddcabcb6dec25d060a3f6bc72cec07b4878bd3f3
Victor hugo Arbesu <victorhugo@MacBook-Air-de-Victor.local>
2024-05-27 17:21:26 -0300

    Criando componente Header

---
+++ src/app/components/Header/index.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './styles.css'; 
+
+const Header = () => {
+    return (
+          <nav className='container'>
+             <img src="logo.svg" alt="" className='logo'/>
+                <ul>
+                    <li>Conservação</li>
+                    <li>Sobre nós</li>
+                    <li>Contato</li>
+                    <li>Login</li>
+                    <li><button className='btn'>Cadastrar</button></li>
+                </ul>
+            </nav>
+        
+    );
+}
+
+export default Header;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,9 @@
-import Image from "next/image";
+
 
 export default function Home() {
   return (
-    <main> 
-    </main>
+    <div>
+      
+    </div>
   );
 }
