400897584dbaf2e2f53f68b4c6f1b3a4fe139263
Victor hugo Arbesu <victorhugo@MacBook-Air-de-Victor.local>
2024-05-29 15:11:16 -0300

    Criando Footer

---
+++ src/app/components/Footer/index.tsx
@@ -0,0 +1,27 @@
+
+import React from 'react';
+import './styles.css';
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="participant">
+        <h3>Victor Hugo Zambelli Arbesu</h3>
+        <p>RM: 553108</p>
+        <p>1TDSPC-2023</p>
+      </div>
+      <div className="participant">
+        <h3>Jõao Pedro Fontana Villagra</h3>
+        <p>RM: 553343</p>
+        <p>1TDSPC-2023</p>
+      </div>
+      <div className="participant">
+        <h3>Vinicius Malavia Lorenzetti</h3>
+        <p>RM: 553121</p>
+        <p>1TDSPC-2023</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 
 
@@ -24,7 +25,7 @@ export default function RootLayout({
         className={inter.className}>
         <Header />
         {children}
-      
+        <Footer />
         </body>
     </html>
   );
